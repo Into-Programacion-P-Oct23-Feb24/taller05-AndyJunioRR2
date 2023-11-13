1c43c054d8e6c49305a1337545cb6e4da0623875
AndyJunioRR2 <andyvein20@gmail.com>
2023-11-12 21:14:46 -0500

    Taller 05

---
+++ manejo-entrada-salida/problema01/Problema01/src/problema01/Problema01.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package problema01;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class Problema01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner entrada = new Scanner(System.in);
+        entrada.useLocale(Locale.US);
+        
+        double base;
+        double altura;
+        double area;
+        System.out.println("Ingrese la base del triangulo");
+        base = entrada.nextDouble();
+        System.out.println("Ingrese la altura del triangulo");
+        altura = entrada.nextDouble();
+        
+        area = base * altura / 2;
+        System.out.printf("El area del triangulo es : %.2f\n", area);
+    }
+}
